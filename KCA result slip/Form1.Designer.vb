d874dbb3a4b55cb43e3439d9cedadca369f94ece
maina <mainajohn5100@gmail.com>
2024-11-21 18:28:10 +0300

    Add project files.

---
+++ KCA result slip/Form1.Designer.vb
@@ -0,0 +1,533 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        txtFinancialAccounting = New TextBox()
+        txtECommerce = New TextBox()
+        txtAppProgramming = New TextBox()
+        txtAI = New TextBox()
+        txtDataStructures = New TextBox()
+        txtGradeECommerce = New TextBox()
+        txtGradeAppProgramming = New TextBox()
+        txtGradeAI = New TextBox()
+        txtGradeDataStructures = New TextBox()
+        txtRegNo = New TextBox()
+        txtName = New TextBox()
+        cmbDepartment = New TextBox()
+        txtGradeFinancialAccounting = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        btnCalculate = New Button()
+        btnPrint = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        Label18 = New Label()
+        PictureBox1 = New PictureBox()
+        PrintPreviewDialog1 = New PrintPreviewDialog()
+        Label19 = New Label()
+        txtTotalMarks = New TextBox()
+        txtPercentage = New TextBox()
+        Label20 = New Label()
+        Label21 = New Label()
+        txtGPA = New TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtFinancialAccounting
+        ' 
+        txtFinancialAccounting.Location = New Point(486, 432)
+        txtFinancialAccounting.Name = "txtFinancialAccounting"
+        txtFinancialAccounting.Size = New Size(125, 27)
+        txtFinancialAccounting.TabIndex = 25
+        ' 
+        ' txtECommerce
+        ' 
+        txtECommerce.Location = New Point(486, 389)
+        txtECommerce.Name = "txtECommerce"
+        txtECommerce.Size = New Size(125, 27)
+        txtECommerce.TabIndex = 24
+        ' 
+        ' txtAppProgramming
+        ' 
+        txtAppProgramming.Location = New Point(486, 348)
+        txtAppProgramming.Name = "txtAppProgramming"
+        txtAppProgramming.Size = New Size(125, 27)
+        txtAppProgramming.TabIndex = 23
+        ' 
+        ' txtAI
+        ' 
+        txtAI.Location = New Point(486, 304)
+        txtAI.Name = "txtAI"
+        txtAI.Size = New Size(125, 27)
+        txtAI.TabIndex = 22
+        ' 
+        ' txtDataStructures
+        ' 
+        txtDataStructures.Location = New Point(486, 260)
+        txtDataStructures.Name = "txtDataStructures"
+        txtDataStructures.Size = New Size(125, 27)
+        txtDataStructures.TabIndex = 21
+        ' 
+        ' txtGradeECommerce
+        ' 
+        txtGradeECommerce.Location = New Point(690, 389)
+        txtGradeECommerce.Name = "txtGradeECommerce"
+        txtGradeECommerce.ReadOnly = True
+        txtGradeECommerce.Size = New Size(227, 27)
+        txtGradeECommerce.TabIndex = 29
+        ' 
+        ' txtGradeAppProgramming
+        ' 
+        txtGradeAppProgramming.Location = New Point(690, 348)
+        txtGradeAppProgramming.Name = "txtGradeAppProgramming"
+        txtGradeAppProgramming.ReadOnly = True
+        txtGradeAppProgramming.Size = New Size(227, 27)
+        txtGradeAppProgramming.TabIndex = 28
+        ' 
+        ' txtGradeAI
+        ' 
+        txtGradeAI.Location = New Point(690, 304)
+        txtGradeAI.Name = "txtGradeAI"
+        txtGradeAI.ReadOnly = True
+        txtGradeAI.Size = New Size(227, 27)
+        txtGradeAI.TabIndex = 27
+        ' 
+        ' txtGradeDataStructures
+        ' 
+        txtGradeDataStructures.Location = New Point(690, 260)
+        txtGradeDataStructures.Name = "txtGradeDataStructures"
+        txtGradeDataStructures.ReadOnly = True
+        txtGradeDataStructures.Size = New Size(227, 27)
+        txtGradeDataStructures.TabIndex = 26
+        ' 
+        ' txtRegNo
+        ' 
+        txtRegNo.Location = New Point(699, 57)
+        txtRegNo.Name = "txtRegNo"
+        txtRegNo.Size = New Size(254, 27)
+        txtRegNo.TabIndex = 35
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(699, 23)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(254, 27)
+        txtName.TabIndex = 34
+        ' 
+        ' cmbDepartment
+        ' 
+        cmbDepartment.Location = New Point(699, 92)
+        cmbDepartment.Name = "cmbDepartment"
+        cmbDepartment.Size = New Size(254, 27)
+        cmbDepartment.TabIndex = 36
+        ' 
+        ' txtGradeFinancialAccounting
+        ' 
+        txtGradeFinancialAccounting.Location = New Point(690, 432)
+        txtGradeFinancialAccounting.Name = "txtGradeFinancialAccounting"
+        txtGradeFinancialAccounting.ReadOnly = True
+        txtGradeFinancialAccounting.Size = New Size(227, 27)
+        txtGradeFinancialAccounting.TabIndex = 30
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(38, 260)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 20)
+        Label1.TabIndex = 37
+        Label1.Text = "BBIT 2201"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(38, 307)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(75, 20)
+        Label2.TabIndex = 38
+        Label2.Text = "BBIT 2202"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(38, 351)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(75, 20)
+        Label3.TabIndex = 39
+        Label3.Text = "BBIT 2203"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(38, 392)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(75, 20)
+        Label4.TabIndex = 40
+        Label4.Text = "BBIT 2204"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(38, 435)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(75, 20)
+        Label5.TabIndex = 41
+        Label5.Text = "BBIT 2205"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(202, 260)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(216, 20)
+        Label6.TabIndex = 42
+        Label6.Text = "Data Structures and Algorithms"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(202, 307)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(146, 20)
+        Label7.TabIndex = 43
+        Label7.Text = "Artificial Intelligence"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(202, 351)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(181, 20)
+        Label8.TabIndex = 44
+        Label8.Text = "Application Programming"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(202, 392)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(95, 20)
+        Label9.TabIndex = 45
+        Label9.Text = "E-Commerce"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(202, 435)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(146, 20)
+        Label10.TabIndex = 46
+        Label10.Text = "Financial Accounting"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(38, 222)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(75, 20)
+        Label11.TabIndex = 47
+        Label11.Text = "Unit Code"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(268, 222)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(80, 20)
+        Label12.TabIndex = 48
+        Label12.Text = "Unit Name"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(486, 222)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(112, 20)
+        Label13.TabIndex = 49
+        Label13.Text = "Acquired Marks"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(734, 222)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(49, 20)
+        Label14.TabIndex = 50
+        Label14.Text = "Grade"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(595, 26)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(49, 20)
+        Label15.TabIndex = 51
+        Label15.Text = "Name"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(595, 57)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(59, 20)
+        Label16.TabIndex = 52
+        Label16.Text = "Reg No"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Location = New Point(595, 95)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(42, 20)
+        Label17.TabIndex = 53
+        Label17.Text = "Dept"
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(666, 552)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(94, 29)
+        btnCalculate.TabIndex = 54
+        btnCalculate.Text = "Results"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnPrint
+        ' 
+        btnPrint.Location = New Point(666, 602)
+        btnPrint.Name = "btnPrint"
+        btnPrint.Size = New Size(94, 29)
+        btnPrint.TabIndex = 55
+        btnPrint.Text = "Print"
+        btnPrint.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(837, 552)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(94, 29)
+        btnClear.TabIndex = 56
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(837, 602)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(94, 29)
+        btnExit.TabIndex = 57
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Location = New Point(222, 64)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(146, 20)
+        Label18.TabIndex = 58
+        Label18.Text = "RESULT SLIP SYSTEM"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(200, 199)
+        PictureBox1.TabIndex = 59
+        PictureBox1.TabStop = False
+        ' 
+        ' PrintPreviewDialog1
+        ' 
+        PrintPreviewDialog1.AutoScrollMargin = New Size(0, 0)
+        PrintPreviewDialog1.AutoScrollMinSize = New Size(0, 0)
+        PrintPreviewDialog1.ClientSize = New Size(400, 300)
+        PrintPreviewDialog1.Enabled = True
+        PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), Icon)
+        PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        PrintPreviewDialog1.Visible = False
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Location = New Point(206, 559)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(42, 20)
+        Label19.TabIndex = 60
+        Label19.Text = "Total"
+        ' 
+        ' txtTotalMarks
+        ' 
+        txtTotalMarks.Location = New Point(268, 552)
+        txtTotalMarks.Name = "txtTotalMarks"
+        txtTotalMarks.ReadOnly = True
+        txtTotalMarks.Size = New Size(123, 27)
+        txtTotalMarks.TabIndex = 61
+        ' 
+        ' txtPercentage
+        ' 
+        txtPercentage.Location = New Point(268, 604)
+        txtPercentage.Name = "txtPercentage"
+        txtPercentage.ReadOnly = True
+        txtPercentage.Size = New Size(125, 27)
+        txtPercentage.TabIndex = 62
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Location = New Point(206, 611)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(49, 20)
+        Label20.TabIndex = 63
+        Label20.Text = "Credit"
+        ' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Location = New Point(206, 673)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(36, 20)
+        Label21.TabIndex = 64
+        Label21.Text = "GPA"
+        ' 
+        ' txtGPA
+        ' 
+        txtGPA.Location = New Point(266, 666)
+        txtGPA.Name = "txtGPA"
+        txtGPA.ReadOnly = True
+        txtGPA.Size = New Size(125, 27)
+        txtGPA.TabIndex = 65
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1052, 755)
+        Controls.Add(txtGPA)
+        Controls.Add(Label21)
+        Controls.Add(Label20)
+        Controls.Add(txtPercentage)
+        Controls.Add(txtTotalMarks)
+        Controls.Add(Label19)
+        Controls.Add(Label18)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnPrint)
+        Controls.Add(btnCalculate)
+        Controls.Add(Label17)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(cmbDepartment)
+        Controls.Add(txtRegNo)
+        Controls.Add(txtName)
+        Controls.Add(txtGradeFinancialAccounting)
+        Controls.Add(txtGradeECommerce)
+        Controls.Add(txtGradeAppProgramming)
+        Controls.Add(txtGradeAI)
+        Controls.Add(txtGradeDataStructures)
+        Controls.Add(txtFinancialAccounting)
+        Controls.Add(txtECommerce)
+        Controls.Add(txtAppProgramming)
+        Controls.Add(txtAI)
+        Controls.Add(txtDataStructures)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "KCA Result slip system"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txtFinancialAccounting As TextBox
+    Friend WithEvents txtECommerce As TextBox
+    Friend WithEvents txtAppProgramming As TextBox
+    Friend WithEvents txtAI As TextBox
+    Friend WithEvents txtDataStructures As TextBox
+    Friend WithEvents txtGradeECommerce As TextBox
+    Friend WithEvents txtGradeAppProgramming As TextBox
+    Friend WithEvents txtGradeAI As TextBox
+    Friend WithEvents txtGradeDataStructures As TextBox
+    Friend WithEvents txtRegNo As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents cmbDepartment As TextBox
+    Friend WithEvents txtGradeFinancialAccounting As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label18 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents Label19 As Label
+    Friend WithEvents txtTotalMarks As TextBox
+    Friend WithEvents txtPercentage As TextBox
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents txtGPA As TextBox
+
+End Class
---
+++ KCA result slip/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Drawing.Printing
+
+Public Class Form1
+    ' Declare a PrintDocument object
+    Private WithEvents printDoc As New PrintDocument
+    Private printContent As String
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Variables for marks
+        Dim dataStructures As Integer = CInt(txtDataStructures.Text)
+        Dim artificialIntelligence As Integer = CInt(txtAI.Text)
+        Dim appProgramming As Integer = CInt(txtAppProgramming.Text)
+        Dim eCommerce As Integer = CInt(txtECommerce.Text)
+        Dim financialAccounting As Integer = CInt(txtFinancialAccounting.Text)
+
+        ' Calculate totals
+        Dim totalMarks As Integer = dataStructures + artificialIntelligence + appProgramming + eCommerce + financialAccounting
+        Dim percentage As Decimal = (totalMarks / 500D) * 100
+        Dim gpa As Decimal = (totalMarks / 500D) * 4.0 ' Assuming a scale of 4.0
+
+        ' Display results
+        txtTotalMarks.Text = totalMarks.ToString()
+        txtPercentage.Text = percentage.ToString("0.00")
+        txtGPA.Text = gpa.ToString("0.00")
+
+        ' Assign grades
+        txtGradeDataStructures.Text = AssignGrade(dataStructures)
+        txtGradeAI.Text = AssignGrade(artificialIntelligence)
+        txtGradeAppProgramming.Text = AssignGrade(appProgramming)
+        txtGradeECommerce.Text = AssignGrade(eCommerce)
+        txtGradeFinancialAccounting.Text = AssignGrade(financialAccounting)
+    End Sub
+
+    Private Function AssignGrade(marks As Integer) As String
+        Select Case marks
+            Case >= 90 : Return "HIGH DISTINCTION"
+            Case >= 80 : Return "DISTINCTION"
+            Case >= 70 : Return "CREDIT"
+            Case >= 60 : Return "PASS"
+            Case Else : Return "FAIL"
+        End Select
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear all input and output fields
+        txtDataStructures.Clear()
+        txtAI.Clear()
+        txtAppProgramming.Clear()
+        txtECommerce.Clear()
+        txtFinancialAccounting.Clear()
+        txtTotalMarks.Clear()
+        txtPercentage.Clear()
+        txtGradeDataStructures.Clear()
+        txtGradeAI.Clear()
+        txtGradeAppProgramming.Clear()
+        txtGradeECommerce.Clear()
+        txtGradeFinancialAccounting.Clear()
+        txtGPA.Clear()
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        ' Prepare content for printing
+        printContent = "KCA UNIVERSITY - RESULT SLIP" & Environment.NewLine &
+                       "-----------------------------------" & Environment.NewLine & Environment.NewLine &
+                       "Name: " & txtName.Text & Environment.NewLine &
+                       "Reg No: " & txtRegNo.Text & Environment.NewLine &
+                       "Department: " & cmbDepartment.Text & Environment.NewLine & Environment.NewLine &
+                       "-----------------------------------" & Environment.NewLine & Environment.NewLine &
+                       "GPA: " & txtGPA.Text & Environment.NewLine &
+                       "-----------------------------------" & Environment.NewLine & Environment.NewLine &
+                       "Course: Data Structures and Algorithms" & Environment.NewLine &
+                       "Marks: " & txtDataStructures.Text & " Grade: " & txtGradeDataStructures.Text & Environment.NewLine &
+                       "Course: Artificial Intelligence" & Environment.NewLine &
+                       "Marks: " & txtAI.Text & " Grade: " & txtGradeAI.Text & Environment.NewLine &
+                       "Course: Application Programming" & Environment.NewLine &
+                       "Marks: " & txtAppProgramming.Text & " Grade: " & txtGradeAppProgramming.Text & Environment.NewLine &
+                       "Course: E-Commerce" & Environment.NewLine &
+                       "Marks: " & txtECommerce.Text & " Grade: " & txtGradeECommerce.Text & Environment.NewLine &
+                       "Course: Financial Accounting" & Environment.NewLine &
+                       "Marks: " & txtFinancialAccounting.Text & " Grade: " & txtFinancialAccounting.Text & Environment.NewLine & Environment.NewLine &
+                       "-----------------------------------" & Environment.NewLine & Environment.NewLine &
+                       "Total Marks: " & txtTotalMarks.Text & Environment.NewLine &
+                       "Percentage: " & txtPercentage.Text
+
+        ' Print the document
+        PrintPreviewDialog1.Document = printDoc
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    Private Sub printDoc_PrintPage(sender As Object, e As PrintPageEventArgs) Handles printDoc.PrintPage
+        ' Set the content to print on the page
+        e.Graphics.DrawString(printContent, New Font("Arial", 12), Brushes.Black, 50, 50)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Exit the application
+        Me.Close()
+    End Sub
+End Class
